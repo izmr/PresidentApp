9a4c3a95ca592e2abdd49f11c350537ddca0571d
marimofire <marimofire@gmail.com>
2012-02-11 15:52:41 +0900

    facebook api

--- public_html/top/index.php
+++ public_html/top/index.php
@@ -1,3 +1,15 @@
 <?php
+require '../vendor/php-sdk/src/facebook.php';
 
-echo 'index';
+$facebook = new Facebook(array(
+  'appId' => '346346428729240',
+  'secret' => 'ea5fd9a005c0becd9e5277864bdf5417'
+));
+
+$user = $facebook->getUser();
+
+if ( $user ) {
+  echo "login!";
+} else {
+  $loginUrl = $facebook->getLoginUrl();
+}
