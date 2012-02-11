a04317e11ba3d2f3c846d55b03013dde23e1e241
unknown <zzz@zzz-PC.(none)>
2012-02-12 01:57:59 +0900

    C部分を_Controllerに分解＆add修正

--- public_html/princess/add.php
+++ public_html/princess/add.php
@@ -1,5 +1,6 @@
 <?php
 require_once("../vendor/php-sdk/src/facebook.php");
+require dirname(__FILE__) . '/../model/President.php';
 
 $appId = '346346428729240';
 $secret = 'ea5fd9a005c0becd9e5277864bdf5417';
@@ -17,9 +18,30 @@ $statusParam = array(
 ?>
 
 <?php
-// 
+
+// 姫登録
+$fql = 'SELECT uid FROM user WHERE uid = me()';
+//$fql = 'SELECT me()';
+$me = $facebook->api(array('method' => 'fql.query', 'query' => $fql));
+
+echo $me[0]['uid'];
+
+//echo $_GET['princess_id'];
+
+$Model = new President();
+$data = array( 'princess_id' => $_GET['princess_id'] );
+$condition = array( 'facebook_id' => $me[0]['uid'] );
+$result = $Model->update( $data, $condition );
+
+//echo '<br />-----------------------------<br />';
+//echo var_dump($data);
+//echo '<br />-----------------------------<br />';
+//echo var_dump($condition);
+//echo '<br />-----------------------------<br />';
+//echo var_dump($result);
+
 
 // リダイレクト
 header("HTTP/1.1 301 Moved Permanently");
-header("Location: /battle");
+header("Location: /president");
 ?>
---
+++ public_html/princess/index_controller.php
@@ -0,0 +1,56 @@
+<?php
+require_once( dirname(__FILE__) . '/../facebook.php' );
+require_once( dirname(__FILE__) . "/../model/Princess.php");
+
+// princessの構成
+$fql = 'SELECT uid,pic,name FROM user WHERE uid IN ( SELECT uid2 FROM friend WHERE uid1 = me() )';
+//echo $fql . "<br />";
+$princesses = $facebook->api(array('method' => 'fql.query', 'query' => $fql));
+
+//echo '<br />!!!!!!!!!!!!!!!!@@@@@@@@@@@@@@@@======================<br />';
+
+// 【DB】
+// 既にDBに登録されている姫：
+//   情報（Scoreとか）を引っ張ってくる
+// DBに未登録の姫：
+//   登録する
+$Model = new Princess();
+for ( $i=0; $i<count($princesses); ++$i ) {
+	$resultRef = $Model->findBy(array('facebook_id' => $princesses[$i]['uid']));
+	if ( $resultRef->num_rows != 0 ) {	// 登録されてる姫
+		$result = $resultRef->fetch_assoc();
+		//var_dump($result);
+		
+		// 姫のDB値設定
+		$level       = $result['level'];
+		$score       = $result['score'];
+		$next_score  = $result['next_score'];
+	} else {	// 登録されていない姫
+		// DBへInsert
+		$data = array();
+		$data['facebook_id'] = $princesses[$i]['uid'];
+		$data['name']        = $princesses[$i]['name'];
+		$data['level']       = 1;
+		$data['score']       = 0;
+		$data['next_score']  = 100;
+		$data['pic']         = $princesses[$i]['pic'];
+		$Model->insert($data);
+		//var_dump($data);
+		
+		// 姫のデフォルト値設定
+		$level       = 1;
+		$score       = 0;
+		$next_score  = 100;
+	}
+
+	// 姫のDB値設定
+	$princesses[$i]['level']       = $level;
+	$princesses[$i]['score']       = $score;
+	$princesses[$i]['next_score']  = $next_score;
+}
+
+//echo '<br />!!!!!!!!!!!!!!!!@@@@@@@@@@@@@@@@======================<br />';
+
+?>
+
+
