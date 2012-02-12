c62b7dd6af24310bcfed1ae6a870acae72c10aa8
Shogo Iwano <shiwano@gmail.com>
2012-02-12 15:07:57 +0900

    Add some effects and Cant click during animation

--- public_html/assets/js/battle.js
+++ public_html/assets/js/battle.js
@@ -1,5 +1,37 @@
 (function ($) {
   var json = {"used_money":68,"damage":[91,27,32],"score":0, 'expend_money': 5, 'next_money': 2000000};
+  var isAnimating = false;
+
+  var calcTotalDamage = function (damage) {
+    var sum = 0;
+    for (var i = 0; i < damage.length ; i++) sum += damage[i];
+    return sum;
+  };
+
+  var showDamageMessage = function (damage, duration) {
+     var message = $('#damage-message');
+    message.find('.value').text(damage);
+    message.fadeIn(duration, function () {
+      message.fadeOut(duration);
+    });
+  };
+
+  var showEffect = function () {
+    switch (Math.floor(Math.random() * 5)) {
+      case 1:
+        heart();
+        break;
+      case 2:
+        angry();
+        break;
+      case 3:
+        coin();
+        break;
+      default:
+        happy();
+        break;
+    }
+  };
 
   var animationAttack = function (index, damage) {
     var card = $('#follower' + (index + 1));
@@ -10,38 +42,57 @@
       'top': '30px',
       'left': '103px'
     }, 200, 'swing', function () {
-      var message = $('#damage-message');
-      message.find('.value').text(damage[index]);
-      message.fadeIn(170, function () {
-        message.fadeOut(170);
-      });
+      showDamageMessage(damage[index], 170);
     });
     card.animate({
       'top': top,
       'left': left
     }, 200, 'swing', function () {
-      if (index < 3) {
+      if (index < 2) {
         animationAttack(index + 1, damage);
+      } else {
+        isAnimating = false;
       }
     });
   };
 
   $(document).on('click', '#attack-button', function (event) {
-    
-    $.getJSON('/heaven/attack.php', function (json) {
-      animationAttack(0, json.damage);
-      $('#score .value').text(json.score);
-      $('#score .next').text(json.next_score);
-      $('#expend-money .value').text(json.expend_money);
-      var money = $('#total-money .value');
-      money.text(money.text() - json.used_money);
-    });
+    if (!isAnimating) {
+      $.getJSON('/heaven/attack.php', function (json) {
+        isAnimating = true;
+
+        if (Math.floor(Math.random() * 3) == 1) {
+          animationAttack(0, json.damage);
+        } else {
+          var i = Math.floor(Math.random() * 4);
+
+          if (i == 1) {
+            animateTotalAttack1(json.damage);
+          } else if (i == 2) {
+            animateTotalAttack2(json.damage);
+          } else {
+            animateTotalAttack3(json.damage);
+          }
+        }
+
+        $('#score .value').text(json.score);
+        $('#score .next').text(json.next_score);
+        $('#expend-money .value').text(json.expend_money);
+        var money = $('#total-money .value');
+        money.text(money.text() - json.used_money);
+      });
+    }
+
     event.preventDefault();
   });
 
+  // 合体攻撃
   var animateTotalAttack1 = function (damage) {
     $("#follower1").animate({top: "-20px" , left: "100px"});
-    $("#follower1").animate({top: "165px" , left: "10px"});
+    $("#follower1").animate({top: "165px" , left: "10px"}, function () {
+      showDamageMessage(calcTotalDamage(damage), 400);
+      showEffect();
+    });
 
     $("#follower2").animate({top: "-20px" , left: "100px"});
     $("#follower2").animate({top: "165px" , left: "10px"});
@@ -54,7 +105,10 @@
 
   var animateTotalAttack2 = function (damage) {
     $("#follower1").animate({top: "-20px" , left: "100px"});
-    $("#follower1").animate({top: "165px" , left: "10px"});
+    $("#follower1").animate({top: "165px" , left: "10px"}, function () {
+      showDamageMessage(calcTotalDamage(damage), 400);
+      showEffect();
+    });
 
     $("#follower2").animate({top: "-20px" , left: "100px"});
     $("#follower2").animate({top: "165px" , left: "103px"});
@@ -67,7 +121,10 @@
   var animateTotalAttack3 = function (damage) {
     $("#follower1").animate({top: "-100px"});
     $("#follower1").animate({top: "165px" , left: "198px"});
-    $("#follower1").animate({left: "10px"});
+    $("#follower1").animate({left: "10px"}, function () {
+      showDamageMessage(calcTotalDamage(damage), 400);
+      showEffect();
+    });
 
     $("#follower2").animate({top: "60px"});
     $("#follower2").animate({top: "165px"});
@@ -76,5 +133,57 @@
     $("#follower3").animate({top: "165px" , left: "10px"});
     $("#follower3").animate({left: "198px"});
   };
+
+  // ❤がくるくる回る
+  function heart(){
+    $("#princess").append("<div id='heartAttack'><ul class='none'><li id='check'>❤</li><li>❤</li><li>❤</li></ul></div>");
+    //$(".happy").css("-webkit-animation-play-state", "running");
+    $("#check").bind("webkitAnimationEnd",function(){
+      $("#heartAttack").remove();
+      isAnimating = false;
+    });
+  }
+
+  // coinが上から降ってくる
+  function coin(){
+    $("#princess").append("<div id='coinAttack'><ul class='none'><li id='check'><img src='img/coin.jpeg'></li></ul></div>");
+    $("#check").bind("webkitAnimationEnd",function(){
+      $("#coinAttack").remove();
+      isAnimating = false;
+    });
+    // $("#princess").addClass("coin");
+    // $(".coin").css("-webkit-animation-play-state", "running");
+    setInterval(addCoin, 500);
+  }
+
+  var coinIndex=0;
+  function addCoin(){
+    $(".none").append("<li><img style='left: " + (Math.random()*100 -30)  + "px' src='img/coin.jpeg'></li>");
+    if(coinIndex<5){
+      coinIndex+=1;
+      setInterval(addCoin, Math.random() * 1000 + 100);
+    }
+  }
+
+  // 怒る
+  function angry() {
+    $("#princess").addClass("angry");
+    $("#princess").append("<div class='condition'>!!</div>");
+    $("#princess").bind("webkitAnimationEnd", function(){
+      $('.condition').remove();
+      $(this).removeClass('angry');
+      isAnimating = false;
+    });
+  }
+
+  function happy() {
+    $("#princess").addClass("happy");
+    $("#princess").append("<div class='condition'>❤</div>");
+    $("#princess").bind("webkitAnimationEnd", function(){
+      $('.condition').remove();
+      $(this).removeClass('happy');
+      isAnimating = false;
+    });
+  }
 })(jQuery);
 
