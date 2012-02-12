84118f4272227ff1019729b2fcc5fef8769b7374
Shogo Iwano <shiwano@gmail.com>
2012-02-12 15:35:56 +0900

    Recover assets/js/follower.js

--- public_html/assets/js/follower.js
+++ public_html/assets/js/follower.js
@@ -2,30 +2,32 @@
   $(document).on('click', '.followers a', function (event) {
     var name = $(this).find('.name').text();
     var imgPath = $(this).find('.icon').attr('src');
-    var power = $(this).find('.power .value').text();
-    var money = $(this).find('.money .value').text();
-    var id = $(this).find('.facebook-id').text();
+    var power = $(this).find('.power').text();
+    var money = $(this).find('.money').text();
+    var jobName = $(this).find('.jobname').text();;
+    var id = $(this).find('.facebook-id').text()
     var input = $('#followers-select-form').find('input[value=""]:first');
 
     if (input.length) {
+	
       input.attr('value', id);
-      $('.follower-cards li.blank:first').append('' +
-            '<article class="follower-card">' +
+      var target = $('.follower-cards li._blank:first');
+      target.children().remove();
+      target.append('' +
+
+            '<article class="follower-card" style="height:135px;"">' +
               '<h1>' + name + '</h1>' +
               '<img class="icon big" src="' + imgPath + '" />' +
-              '<p class="power">Power: ' + power + '</p>' +
-              '<p class="money">Money: ' + money + '</p>' +
-            '</article>').removeClass('blank');
+              '<p class="power">' + power + '</p>' +
+              '<p class="money">' + money + '</p>' +
+              '<p class="jobName">' + jobName + '</p>' +
+            '</article>').removeClass('_blank');
       $(this).parents('li').remove();
     } else {
       alert('セットできるフォロワーは3人までです');
     }
 
     event.preventDefault();
-  });
-
-  $(document).on('click', '.follower-card', function (event) {
-    $(this).parents('li').remove();
   });
 })(jQuery);
 
